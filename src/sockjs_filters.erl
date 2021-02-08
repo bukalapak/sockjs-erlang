921b5e2edd16a2be4418156366db3b5ace1fcdb9
alpha.ferry <alpha.ferry@bukalapak.com>
2021-02-08 17:22:11 +0700

    formatting

--- src/sockjs_filters.erl
+++ src/sockjs_filters.erl
@@ -1,7 +1,13 @@
 -module(sockjs_filters).
 
--export([cache_for/2, h_no_cache/2, h_sid/2, xhr_cors/2,
-	 xhr_options_get/2, xhr_options_post/2]).
+-export([
+    cache_for/2,
+    h_no_cache/2,
+    h_sid/2,
+    xhr_cors/2,
+    xhr_options_get/2,
+    xhr_options_post/2
+]).
 
 -include("sockjs_internal.hrl").
 
@@ -13,11 +19,14 @@
 
 cache_for(Req, Headers) ->
     Expires =
-	calendar:gregorian_seconds_to_datetime(calendar:datetime_to_gregorian_seconds(calendar:now_to_datetime(now()))
-						 + (?YEAR)),
-    H = [{"Cache-Control",
-	  "public, max-age=" ++ integer_to_list(?YEAR)},
-	 {"Expires", httpd_util:rfc1123_date(Expires)}],
+        calendar:gregorian_seconds_to_datetime(
+            calendar:datetime_to_gregorian_seconds(calendar:now_to_datetime(now())) +
+                (?YEAR)
+        ),
+    H = [
+        {"Cache-Control", "public, max-age=" ++ integer_to_list(?YEAR)},
+        {"Expires", httpd_util:rfc1123_date(Expires)}
+    ],
     {H ++ Headers, Req}.
 
 -spec h_sid(req(), headers()) -> {headers(), req()}.
@@ -28,60 +37,70 @@ h_sid(Req, Headers) ->
     %% set it to a dumb value. It doesn't really matter what, as
     %% session information is usually added by the load balancer.
     C = sockjs_http:jsessionid(Req),
-    H = case C of
-	  undefined ->
-	      [{"Set-Cookie", "JSESSIONID=dummy; path=/"}];
-	  Jsid ->
-	      [{"Set-Cookie", "JSESSIONID=" ++ Jsid ++ "; path=/"}]
-	end,
+    H =
+        case C of
+            undefined ->
+                [{"Set-Cookie", "JSESSIONID=dummy; path=/"}];
+            Jsid ->
+                [{"Set-Cookie", "JSESSIONID=" ++ Jsid ++ "; path=/"}]
+        end,
     H ++ Headers.
 
--spec h_no_cache(req(), headers()) -> {headers(),
-				       req()}.
+-spec h_no_cache(req(), headers()) -> {headers(), req()}.
 
 h_no_cache(Req, Headers) ->
-    H = [{"Cache-Control",
-	  "no-store, no-cache, must-revalidate, "
-	  "max-age=0"}],
+    H = [
+        {"Cache-Control",
+            "no-store, no-cache, must-revalidate, "
+            "max-age=0"}
+    ],
     {H ++ Headers, Req}.
 
 -spec xhr_cors(req(), headers()) -> {headers(), req()}.
 
 xhr_cors(Req, Headers) ->
     {OriginH, Req1} = sockjs_http:header(origin, Req),
-    Origin = case OriginH of
-	       "null" -> "*";
-	       undefined -> "*";
-	       O -> O
-	     end,
+    Origin =
+        case OriginH of
+            "null" -> "*";
+            undefined -> "*";
+            O -> O
+        end,
     {HeadersH, Req2} =
-	sockjs_http:header('access-control-request-headers',
-			   Req1),
-    AllowHeaders = case HeadersH of
-		     undefined -> [];
-		     V -> [{"Access-Control-Allow-Headers", V}]
-		   end,
-    H = [{"Access-Control-Allow-Origin", Origin},
-	 {"Access-Control-Allow-Credentials", "true"}],
+        sockjs_http:header(
+            'access-control-request-headers',
+            Req1
+        ),
+    AllowHeaders =
+        case HeadersH of
+            undefined -> [];
+            V -> [{"Access-Control-Allow-Headers", V}]
+        end,
+    H = [
+        {"Access-Control-Allow-Origin", Origin},
+        {"Access-Control-Allow-Credentials", "true"}
+    ],
     {H ++ AllowHeaders ++ Headers, Req2}.
 
--spec xhr_options_post(req(), headers()) -> {headers(),
-					     req()}.
+-spec xhr_options_post(req(), headers()) -> {headers(), req()}.
 
 xhr_options_post(Req, Headers) ->
     xhr_options(Req, Headers, ["OPTIONS", "POST"]).
 
--spec xhr_options_get(req(), headers()) -> {headers(),
-					    req()}.
+-spec xhr_options_get(req(), headers()) -> {headers(), req()}.
 
 xhr_options_get(Req, Headers) ->
     xhr_options(Req, Headers, ["OPTIONS", "GET"]).
 
--spec xhr_options(req(), headers(),
-		  [string()]) -> {headers(), req()}.
+-spec xhr_options(
+    req(),
+    headers(),
+    [string()]
+) -> {headers(), req()}.
 
 xhr_options(Req, Headers, Methods) ->
-    H = [{"Access-Control-Allow-Methods",
-	  string:join(Methods, ", ")},
-	 {"Access-Control-Max-Age", integer_to_list(?YEAR)}],
+    H = [
+        {"Access-Control-Allow-Methods", string:join(Methods, ", ")},
+        {"Access-Control-Max-Age", integer_to_list(?YEAR)}
+    ],
     {H ++ Headers, Req}.
--- src/sockjs_handler.erl
+++ src/sockjs_handler.erl
@@ -11,50 +11,62 @@
 -include("sockjs_internal.hrl").
 
 -define(SOCKJS_URL,
-	"//cdn.jsdelivr.net/sockjs/1.0.3/sockjs.min.js").
+    "//cdn.jsdelivr.net/sockjs/1.0.3/sockjs.min.js"
+).
 
 %% --------------------------------------------------------------------------
 
--spec init_state(binary(), callback(), any(),
-		 [tuple()]) -> service().
+-spec init_state(
+    binary(),
+    callback(),
+    any(),
+    [tuple()]
+) -> service().
 
 init_state(Prefix, Callback, State, Options) ->
-    #service{prefix = binary_to_list(Prefix),
-	     callback = Callback, state = State,
-	     sockjs_url =
-		 proplists:get_value(sockjs_url, Options, ?SOCKJS_URL),
-	     websocket =
-		 proplists:get_value(websocket, Options, true),
-	     cookie_needed =
-		 proplists:get_value(cookie_needed, Options, false),
-	     disconnect_delay =
-		 proplists:get_value(disconnect_delay, Options, 5000),
-	     heartbeat_delay =
-		 proplists:get_value(heartbeat_delay, Options, 25000),
-	     response_limit =
-		 proplists:get_value(response_limit, Options,
-				     128 * 1024),
-	     logger =
-		 proplists:get_value(logger, Options,
-				     fun default_logger/3),
-	     subproto_pref =
-		 proplists:get_value(subproto_pref, Options)}.
+    #service{
+        prefix = binary_to_list(Prefix),
+        callback = Callback,
+        state = State,
+        sockjs_url =
+            proplists:get_value(sockjs_url, Options, ?SOCKJS_URL),
+        websocket =
+            proplists:get_value(websocket, Options, true),
+        cookie_needed =
+            proplists:get_value(cookie_needed, Options, false),
+        disconnect_delay =
+            proplists:get_value(disconnect_delay, Options, 5000),
+        heartbeat_delay =
+            proplists:get_value(heartbeat_delay, Options, 25000),
+        response_limit =
+            proplists:get_value(
+                response_limit,
+                Options,
+                128 * 1024
+            ),
+        logger =
+            proplists:get_value(
+                logger,
+                Options,
+                fun default_logger/3
+            ),
+        subproto_pref =
+            proplists:get_value(subproto_pref, Options)
+    }.
 
 %% --------------------------------------------------------------------------
 
--spec is_valid_ws(service(), req()) -> {boolean(),
-					tuple()}.
+-spec is_valid_ws(service(), req()) -> {boolean(), tuple()}.
 
 is_valid_ws(Service, Req) ->
     case get_action(Service, Req) of
-      {{match, WS}, Req1}
-	  when WS =:= websocket orelse WS =:= rawwebsocket ->
-	  valid_ws_request(Service, Req1);
-      {_Else, _Req1} -> {false, {}}
+        {{match, WS}, Req1} when WS =:= websocket orelse WS =:= rawwebsocket ->
+            valid_ws_request(Service, Req1);
+        {_Else, _Req1} ->
+            {false, {}}
     end.
 
--spec valid_ws_request(service(), req()) -> {boolean(),
-					     tuple()}.
+-spec valid_ws_request(service(), req()) -> {boolean(), tuple()}.
 
 valid_ws_request(_Service, Req) ->
     R1 = valid_ws_upgrade(Req),
@@ -63,31 +75,36 @@ valid_ws_request(_Service, Req) ->
 
 valid_ws_upgrade(Req) ->
     case sockjs_http:header(upgrade, Req) of
-      undefined -> false;
-      {V, _Req2} ->
-	  case string:to_lower(V) of
-	    "websocket" -> true;
-	    _Else -> false
-	  end
+        undefined ->
+            false;
+        {V, _Req2} ->
+            case string:to_lower(V) of
+                "websocket" -> true;
+                _Else -> false
+            end
     end.
 
 valid_ws_connection(Req) ->
     case sockjs_http:header(connection, Req) of
-      undefined -> false;
-      V ->
-	  Vs = [string:strip(T)
-		|| T <- string:tokens(string:to_lower(V), ",")],
-	  {lists:member("upgrade", Vs), Req}
+        undefined ->
+            false;
+        V ->
+            Vs = [
+                string:strip(T)
+                || T <- string:tokens(string:to_lower(V), ",")
+            ],
+            {lists:member("upgrade", Vs), Req}
     end.
 
--spec get_action(service(), req()) -> nomatch |
-				      {match, atom()}.
+-spec get_action(service(), req()) ->
+    nomatch
+    | {match, atom()}.
 
 get_action(Service, Req) ->
     Dispatch = dispatch_req(Service, Req),
     case Dispatch of
-      {match, {_, Action, _, _, _}} -> {match, Action};
-      _Else -> nomatch
+        {match, {_, Action, _, _, _}} -> {match, Action};
+        _Else -> nomatch
     end.
 
 %% --------------------------------------------------------------------------
@@ -95,21 +112,25 @@ get_action(Service, Req) ->
 strip_prefix(LongPath, Prefix) ->
     {A, B} = lists:split(length(Prefix), LongPath),
     case Prefix of
-      A -> {ok, B};
-      _Any ->
-	  {error,
-	   io_lib:format("Wrong prefix: ~p is not ~p",
-			 [A, Prefix])}
+        A ->
+            {ok, B};
+        _Any ->
+            {error,
+                io_lib:format(
+                    "Wrong prefix: ~p is not ~p",
+                    [A, Prefix]
+                )}
     end.
 
--type dispatch_result() :: nomatch |
-			   {match,
-			    {send | recv | none, atom(), server(), session(),
-			     [atom()]}} |
-			   {bad_method, [atom()]}.
+-type dispatch_result() ::
+    nomatch
+    | {match, {send | recv | none, atom(), server(), session(), [atom()]}}
+    | {bad_method, [atom()]}.
 
--spec dispatch_req(service(),
-		   req()) -> dispatch_result().
+-spec dispatch_req(
+    service(),
+    req()
+) -> dispatch_result().
 
 dispatch_req(#service{prefix = Prefix}, Req) ->
     Method = sockjs_http:method(Req),
@@ -117,27 +138,37 @@ dispatch_req(#service{prefix = Prefix}, Req) ->
     {ok, PathRemainder} = strip_prefix(LongPath, Prefix),
     dispatch(Method, PathRemainder).
 
--spec dispatch(atom(),
-	       nonempty_string()) -> dispatch_result().
+-spec dispatch(
+    atom(),
+    nonempty_string()
+) -> dispatch_result().
 
 dispatch(Method, Path) ->
-    lists:foldl(fun ({Match, MethodFilters}, nomatch) ->
-			case Match(Path) of
-			  nomatch -> nomatch;
-			  [Server, Session] ->
-			      case lists:keyfind(Method, 1, MethodFilters) of
-				false ->
-				    Methods = [K
-					       || {K, _, _, _}
-						      <- MethodFilters],
-				    {bad_method, Methods};
-				{_Method, Type, A, Filters} ->
-				    {match, {Type, A, Server, Session, Filters}}
-			      end
-			end;
-		    (_, Result) -> Result
-		end,
-		nomatch, filters()).
+    lists:foldl(
+        fun
+            ({Match, MethodFilters}, nomatch) ->
+                case Match(Path) of
+                    nomatch ->
+                        nomatch;
+                    [Server, Session] ->
+                        case lists:keyfind(Method, 1, MethodFilters) of
+                            false ->
+                                Methods = [
+                                    K
+                                    || {K, _, _, _} <-
+                                           MethodFilters
+                                ],
+                                {bad_method, Methods};
+                            {_Method, Type, A, Filters} ->
+                                {match, {Type, A, Server, Session, Filters}}
+                        end
+                end;
+            (_, Result) ->
+                Result
+        end,
+        nomatch,
+        filters()
+    ).
 
 %% --------------------------------------------------------------------------
 
@@ -145,38 +176,39 @@ filters() ->
     %OptsFilters = [h_sid, xhr_cors, cache_for, xhr_options_post],
     %% websocket does not actually go via handle_req/3 but we need
     %% something in dispatch/2
-    [{t("/websocket"), [{'GET', none, websocket, []}]},
-     %{t("/xhr_send"),                [{'POST',    recv, xhr_send,       [h_sid, h_no_cache, xhr_cors]},
-     %                                 {'OPTIONS', none, options,        OptsFilters}]},
-     %{t("/xhr"),                     [{'POST',    send, xhr_polling,    [h_sid, h_no_cache, xhr_cors]},
-     %                                 {'OPTIONS', none, options,        OptsFilters}]},
-     %{t("/xhr_streaming"),           [{'POST',    send, xhr_streaming,  [h_sid, h_no_cache, xhr_cors]},
-     %                                 {'OPTIONS', none, options,        OptsFilters}]},
-     %{t("/jsonp_send"),              [{'POST',    recv, jsonp_send,     [h_sid, h_no_cache]}]},
-     %{t("/jsonp"),                   [{'GET',     send, jsonp,          [h_sid, h_no_cache]}]},
-     %{t("/eventsource"),             [{'GET',     send, eventsource,    [h_sid, h_no_cache]}]},
-     %{t("/htmlfile"),                [{'GET',     send, htmlfile,       [h_sid, h_no_cache]}]},
-     {p("/websocket"), [{'GET', none, rawwebsocket, []}]},
-     {p(""), [{'GET', none, welcome_screen, []}]},
-     %{p("/iframe[0-9-.a-z_]*.html"), [{'GET',     none, iframe,         [cache_for]}]},
-     {p("/info"),
-      [{'GET', none, info_test, [h_no_cache, xhr_cors]},
-       {'OPTIONS', none, options,
-	[h_sid, xhr_cors, cache_for, xhr_options_get]}]}].
+    [
+        {t("/websocket"), [{'GET', none, websocket, []}]},
+        %{t("/xhr_send"),                [{'POST',    recv, xhr_send,       [h_sid, h_no_cache, xhr_cors]},
+        %                                 {'OPTIONS', none, options,        OptsFilters}]},
+        %{t("/xhr"),                     [{'POST',    send, xhr_polling,    [h_sid, h_no_cache, xhr_cors]},
+        %                                 {'OPTIONS', none, options,        OptsFilters}]},
+        %{t("/xhr_streaming"),           [{'POST',    send, xhr_streaming,  [h_sid, h_no_cache, xhr_cors]},
+        %                                 {'OPTIONS', none, options,        OptsFilters}]},
+        %{t("/jsonp_send"),              [{'POST',    recv, jsonp_send,     [h_sid, h_no_cache]}]},
+        %{t("/jsonp"),                   [{'GET',     send, jsonp,          [h_sid, h_no_cache]}]},
+        %{t("/eventsource"),             [{'GET',     send, eventsource,    [h_sid, h_no_cache]}]},
+        %{t("/htmlfile"),                [{'GET',     send, htmlfile,       [h_sid, h_no_cache]}]},
+        {p("/websocket"), [{'GET', none, rawwebsocket, []}]},
+        {p(""), [{'GET', none, welcome_screen, []}]},
+        %{p("/iframe[0-9-.a-z_]*.html"), [{'GET',     none, iframe,         [cache_for]}]},
+        {p("/info"), [
+            {'GET', none, info_test, [h_no_cache, xhr_cors]},
+            {'OPTIONS', none, options, [h_sid, xhr_cors, cache_for, xhr_options_get]}
+        ]}
+    ].
 
-p(S) -> fun (Path) -> re(Path, "^" ++ S ++ "[/]?$") end.
+p(S) -> fun(Path) -> re(Path, "^" ++ S ++ "[/]?$") end.
 
 t(S) ->
-    fun (Path) ->
-	    re(Path, "^/([^/.]+)/([^/.]+)" ++ S ++ "[/]?$")
+    fun(Path) ->
+        re(Path, "^/([^/.]+)/([^/.]+)" ++ S ++ "[/]?$")
     end.
 
 re(Path, S) ->
-    case re:run(Path, S, [{capture, all_but_first, list}])
-	of
-      nomatch -> nomatch;
-      {match, []} -> [dummy, dummy];
-      {match, [Server, Session]} -> [Server, Session]
+    case re:run(Path, S, [{capture, all_but_first, list}]) of
+        nomatch -> nomatch;
+        {match, []} -> [dummy, dummy];
+        {match, [Server, Session]} -> [Server, Session]
     end.
 
 %% --------------------------------------------------------------------------
@@ -191,40 +223,63 @@ handle_req(Service = #service{logger = Logger}, Req) ->
 handle(nomatch, _Service, Req) ->
     sockjs_http:reply(404, [], "", Req);
 handle({bad_method, Methods}, _Service, Req) ->
-    MethodsStr = string:join([atom_to_list(M)
-			      || M <- Methods],
-			     ", "),
+    MethodsStr = string:join(
+        [
+            atom_to_list(M)
+            || M <- Methods
+        ],
+        ", "
+    ),
     H = [{"Allow", MethodsStr}],
     sockjs_http:reply(405, H, "", Req);
-handle({match,
-	{Type, Action, _Server, Session, Filters}},
-       Service, Req) ->
-    {Headers, Req2} = lists:foldl(fun (Filter,
-				       {Headers0, Req1}) ->
-					  sockjs_filters:Filter(Req1, Headers0)
-				  end,
-				  {[], Req}, Filters),
+handle(
+    {match, {Type, Action, _Server, Session, Filters}},
+    Service,
+    Req
+) ->
+    {Headers, Req2} = lists:foldl(
+        fun(
+            Filter,
+            {Headers0, Req1}
+        ) ->
+            sockjs_filters:Filter(Req1, Headers0)
+        end,
+        {[], Req},
+        Filters
+    ),
     case Type of
-      send ->
-	  Info = extract_info(Req2),
-	  _SPid = sockjs_session:maybe_create(Session, Service,
-					      Info),
-	  sockjs_action:Action(Req2, Headers, Service, Session);
-      recv ->
-	  try sockjs_action:Action(Req2, Headers, Service,
-				   Session)
-	  catch
-	    no_session ->
-		H = sockjs_filters:h_sid(Req2, []),
-		sockjs_http:reply(404, H, "", Req2)
-	  end;
-      none -> sockjs_action:Action(Req2, Headers, Service)
+        send ->
+            Info = extract_info(Req2),
+            _SPid = sockjs_session:maybe_create(
+                Session,
+                Service,
+                Info
+            ),
+            sockjs_action:Action(Req2, Headers, Service, Session);
+        recv ->
+            try
+                sockjs_action:Action(
+                    Req2,
+                    Headers,
+                    Service,
+                    Session
+                )
+            catch
+                no_session ->
+                    H = sockjs_filters:h_sid(Req2, []),
+                    sockjs_http:reply(404, H, "", Req2)
+            end;
+        none ->
+            sockjs_action:Action(Req2, Headers, Service)
     end.
 
 %% --------------------------------------------------------------------------
 
--spec default_logger(service(), req(),
-		     websocket | http) -> no_return().
+-spec default_logger(
+    service(),
+    req(),
+    websocket | http
+) -> no_return().
 
 default_logger(_Service, Req, _Type) ->
     % As the service need a function to call as default, we simply give a dummy function
@@ -239,14 +294,26 @@ extract_info(Req) ->
     Peer = sockjs_http:peername(Req),
     Sock = sockjs_http:sockname(Req),
     Path = sockjs_http:path(Req),
-    Headers = lists:foldl(fun (H, Acc) ->
-				  case sockjs_http:header(H, Req) of
-				    undefined -> Acc;
-				    V -> [{H, V} | Acc]
-				  end
-			  end,
-			  [],
-			  [referer, 'x-client-ip', 'x-forwarded-for',
-			   'x-cluster-client-ip', via, 'x-real-ip']),
-    [{peername, Peer}, {sockname, Sock}, {path, Path},
-     {headers, Headers}].
+    Headers = lists:foldl(
+        fun(H, Acc) ->
+            case sockjs_http:header(H, Req) of
+                undefined -> Acc;
+                V -> [{H, V} | Acc]
+            end
+        end,
+        [],
+        [
+            referer,
+            'x-client-ip',
+            'x-forwarded-for',
+            'x-cluster-client-ip',
+            via,
+            'x-real-ip'
+        ]
+    ),
+    [
+        {peername, Peer},
+        {sockname, Sock},
+        {path, Path},
+        {headers, Headers}
+    ].
